b4d9b9c22449dc3e543528c46531c606c4a969cf
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-04-25 16:47:05 +0300

    Add model for ObjectStorage delete event

---
+++ Core/Models/ObjectStorageDeleteLog.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Models;
+
+/// <summary>
+/// Keep track of deleted files, the deletion of these should be delayed.
+/// </summary>
+public class ObjectStorageDeleteLog
+{
+    [Key]   
+    public Int64 Id { get; set; }
+    
+    [ForeignKey("FileId")]
+    public ObjectStorageFile? File { get; set; }
+    public Int64 FileId { get; set; }
+    
+    public DateTime DeletedAt { get; set; }
+    public DateTime? ConfirmedAt { get; set; }
+}
