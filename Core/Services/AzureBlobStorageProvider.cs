e2558cc9433f20a32c5517b4a8145f9531eb0d73
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-04-26 15:54:34 +0300

    Implement RestorationService using slow method

---
+++ Core/Services/AzureBlobStorageProvider.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using Core.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Services;
+
+public class AzureBlobStorageProvider : IObjectStorageProvider
+{
+    private readonly IConfiguration _configuration;
+    
+    public AzureBlobStorageProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public Task RestoreFile(ObjectStorageFile file)
+    {
+        throw new NotImplementedException();
+    }
+}
