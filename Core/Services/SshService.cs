1842b29386406268f1a56c9cb7c8e2e43b073752
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-06-19 11:08:41 +0300

    Add a helper class for parsin SSH config file

---
+++ Core/Services/SshService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Services.Interfaces;
+using Core.Services.ServiceDataModels;
+
+namespace Core.Services;
+
+public class SshService : ISshService
+{
+    /// <inheritdoc />
+    public SshConfig ReadSshConfigFile()
+    {
+        return new SshConfig();
+    }
+}
