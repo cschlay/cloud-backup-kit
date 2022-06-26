2a654ef05d51cf5fa09b2ffd5bc0a562f9a5875a
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-06-26 11:36:00 +0300

    Add CLI command to backup a database

--- ConsoleTools/Program.cs
+++ ConsoleTools/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using ConsoleTools;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+switch (args[0])
+{
+    case SqlDumpCommand.Command: 
+        SqlDumpCommand.Run(configuration["SqlDump:Directory"], args);
+        break;
+}
---
+++ ConsoleTools/SqlDumpCommand.cs
@@ -0,0 +1,19 @@
+﻿using Core.Services;
+
+namespace ConsoleTools;
+
+public static class SqlDumpCommand
+{
+    public const string Command = "sqldump";
+
+    public static void Run(string localDirectory, string[] args)
+    {
+        if (args.Length != 3)
+        {
+            throw new ArgumentException("Invalid number of arguments, expected 'sqldump <sshConfigName> <databaseName>'.");
+        }
+        
+        var sqlDumpService = new SqlDumpService(new SshService());
+        sqlDumpService.CreateDump(args[1], args[2], localDirectory);
+    }
+}
